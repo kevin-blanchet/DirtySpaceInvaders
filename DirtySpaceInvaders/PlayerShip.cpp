8a8f92b9b4061313634b505200194476d7769ac2
BLANCHET Kevin <kevin.blanchet@ynov.com>
2023-03-06 18:01:32 +0100

    refactor: PlayerShip class

---
+++ DirtySpaceInvaders/PlayerShip.cpp
@@ -0,0 +1,39 @@
+#include "PlayerShip.h"
+
+#include <cstring>
+
+#include "Input.h"
+#include "PlayerLaser.h"
+#include "PlayField.h"
+
+PlayerShip::PlayerShip()
+{
+	m_objType = new char[64];
+	strcpy(m_objType, "PlayerShip");
+	sprite = RS_Player;
+}
+
+PlayerShip::~PlayerShip()
+{
+	delete m_objType;
+}
+
+void PlayerShip::Update(PlayField& world)
+{
+	if (world.controllerInput->Left())
+	{
+		pos.x -= 1;
+	}
+	else if (world.controllerInput->Right())
+	{
+		pos.x += 1;
+	}
+
+	if (world.controllerInput->Fire() && world.PlayerLasers > 0)
+	{
+		//Spawn laser
+		GameObject& newLaser = *(new PlayerLaser);
+		newLaser.pos = pos;
+		world.SpawnLaser(&newLaser);
+	}
+}
--- DirtySpaceInvaders/PlayerShip.h
+++ DirtySpaceInvaders/PlayerShip.h
@@ -1,40 +1,15 @@
 #pragma once
-#include <cstring>
 
 #include "GameObject.h"
-#include "PlayerLaser.h"
+
+class PlayField;
 
 class PlayerShip : public GameObject
 {
 public:
-	PlayerShip()
-	{
-		m_objType = new char[64];
-		strcpy(m_objType, "PlayerShip");
-		sprite = RS_Player;
-	}
-	~PlayerShip()
-	{
-		delete m_objType;
-	}
+	PlayerShip();
+	
+	~PlayerShip();
 
-	void Update(PlayField& world)
-	{
-		if (world.controllerInput->Left())
-		{
-			pos.x -= 1;
-		}
-		else if (world.controllerInput->Right())
-		{
-			pos.x += 1;
-		}
-
-		if (world.controllerInput->Fire() && world.PlayerLasers > 0)
-		{
-			//Spawn laser
-			GameObject& newLaser = *(new PlayerLaser);
-			newLaser.pos = pos;
-			world.SpawnLaser(&newLaser);
-		}
-	}
+	void Update(PlayField& world);
 };
