fa26d2a1982ff52f320a8a2cb1e79f0725941b47
BLANCHET Kevin <kevin.blanchet@ynov.com>
2023-03-06 19:04:24 +0100

    fix: set rGen to static

--- DirtySpaceInvaders/Random.h
+++ DirtySpaceInvaders/Random.h
@@ -2,6 +2,6 @@
 
 #include <random>
 
-std::default_random_engine rGen;
+static std::default_random_engine rGen;
 typedef std::uniform_int_distribution<int> intRand;
 typedef std::uniform_real_distribution<float> floatRand;
