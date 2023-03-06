00e192ba5032f9dedb42d1526ce2fb0ee803b403
BLANCHET Kevin <kevin.blanchet@ynov.com>
2023-03-06 18:17:27 +0100

    refactor: Alien class

---
+++ DirtySpaceInvaders/Alien.cpp
@@ -0,0 +1,56 @@
+#include "Alien.h"
+
+#include <cstring>
+
+#include "AlienLaser.h"
+#include "PlayField.h"
+#include "Random.h"
+
+Alien::Alien()
+{
+	m_objType = new char[64];
+	strcpy(m_objType, "AlienShip");
+	sprite = RS_Alien;
+}
+
+Alien::~Alien()
+{
+	delete m_objType;
+}
+
+bool Alien::DecreaseHealth()
+{
+	health -= 1.f;
+	return health <= 0;
+}
+
+void Alien::Update(PlayField& world)
+{
+	pos.x += direction * velocity;
+	// Border check
+	if (pos.x < 0 || pos.x >= world.bounds.x - 1)
+	{
+		direction = -direction;
+		pos.y += 1;
+	}
+
+	// Border check vertical:
+	if (pos.y >= world.bounds.y - 1)
+	{
+		// kill player
+		GameObject* player = world.GetPlayerObject();
+		if (pos.IntCmp(player->pos))
+		{
+			world.RemoveObject(player);
+		}
+	}
+
+	floatRand fireRate(0, 1);
+	if (fireRate(rGen) < 0.5 && world.AlienLasers > 0)
+	{
+		//Spawn laser
+		GameObject& newLaser = *(new AlienLaser);
+		newLaser.pos = pos;
+		world.SpawnLaser(&newLaser);
+	}
+}
--- DirtySpaceInvaders/Alien.h
+++ DirtySpaceInvaders/Alien.h
@@ -1,24 +1,14 @@
 #pragma once
-#include <cstring>
 
-#include "AlienLaser.h"
 #include "GameObject.h"
-#include "Random.h"
 
+class PlayField;
 
 class Alien : public GameObject
 {
 public:
-	Alien()
-	{
-		m_objType = new char[64];
-		strcpy(m_objType, "AlienShip");
-		sprite = RS_Alien;
-	}
-	~Alien()
-	{
-		delete m_objType;
-	}
+	Alien();
+	~Alien();
 
 private:
 	float health = 1.f;
@@ -26,40 +16,7 @@ private:
 	float direction = 1.f;
 	float velocity = 0.5f;
 
-	bool DecreaseHealth()
-	{
-		health -= 1.f;
-		return health <= 0;
-	}
+	bool DecreaseHealth();
 
-	void Update(PlayField& world)
-	{
-		pos.x += direction * velocity;
-		// Border check
-		if (pos.x < 0 || pos.x >= world.bounds.x - 1)
-		{
-			direction = -direction;
-			pos.y += 1;
-		}
-
-		// Border check vertical:
-		if (pos.y >= world.bounds.y - 1)
-		{
-			// kill player
-			GameObject* player = world.GetPlayerObject();
-			if (pos.IntCmp(player->pos))
-			{
-				world.RemoveObject(player);
-			}
-		}
-
-		floatRand fireRate(0, 1);
-		if (fireRate(rGen) < 0.5 && world.AlienLasers > 0)
-		{
-			//Spawn laser
-			GameObject& newLaser = *(new AlienLaser);
-			newLaser.pos = pos;
-			world.SpawnLaser(&newLaser);
-		}
-	}
+	void Update(PlayField& world);
 };
